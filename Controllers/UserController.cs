6b95eada17c7c4a61569766fedcc0d1f381bc30d
RKzeraa <rkzeraa@gmail.com>
2023-05-12 14:14:33 -0300

    [Feat] New Project API User CRUD - V01.00

---
+++ Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Json;
+using System.Xml.Linq;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDUserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<User> users = new List<User>();
+
+        // GET api/<UserController>/5
+        [HttpGet("GetUsers")]
+        public string GetAll()
+        {
+            string userJSON = "Is Empty, Create a new user to Get all users";
+            foreach (User user in users)
+            {
+                if(user != null)
+                {
+                    userJSON += "\n{" + JsonSerializer.Serialize(user.ToString()) + "}";
+                }
+                
+            }
+            string format = userJSON.Replace("Is Empty, Create a new user to Get all users\n", "");
+            format = format.Replace("\n", ", ");
+            //string json = JsonSerializer.Serialize(userJSON);
+            return (format);
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            User user = users.Find(u => u.Id == id)!;
+            string json = JsonSerializer.Serialize(user.ToString());
+            return (json);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post(string name, string email, string birthDate)
+        {
+            User user = new User(name, email, birthDate);
+            users.Add(user);
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, string name, string email, string birthDate)
+        {
+            User user = users.Find(u => u.Id == id)!;
+            user.Name = name;
+            user.Email = email;
+            user.BirthDate = DateTime.Parse(birthDate);
+            user.CreatedUserDate = DateTime.Now;
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            User user = users.Find(u => u.Id == id)!;
+            users.Remove(user);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,28 @@
+﻿namespace CRUDUserAPI
+{
+    public class User
+    {
+        static int nextId = 1;
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedUserDate { get; set; }
+
+        public User(string name, string email, string birthDate)
+        {
+            Id = nextId;
+            Name = name;
+            Email = email;
+            BirthDate = DateTime.Parse(birthDate);
+            CreatedUserDate = DateTime.Now;
+            nextId++;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Email: {Email}, Birthdate: {BirthDate.ToString("d")}, CreatedUserDate: {CreatedUserDate}";
+        }
+    }
+}
